63f829f65686500823ce628b1872e55cfae3548e
Kevin Wong <kkwokwai22@gmail.com>
2017-03-27 23:04:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// set up express app
+const app = express();
+
+
+
+// listening on port
+app.listen(process.env.port || 4000, function(){
+  console.log('listening to port on 4000')
+})
