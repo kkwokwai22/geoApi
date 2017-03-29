4d0346e45fd8575f5bfe3ff4343d4822de2030c8
Kevin Wong <kkwokwai22@gmail.com>
2017-03-28 23:38:54 -0700

    add middleware

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-
+const routes = require('./routes/api');
 // set up express app
 const app = express();
 
-
+// initialize routes
+app.use('/api', routes);
 
 // listening on port
 app.listen(process.env.port || 4000, function(){
   console.log('listening to port on 4000')
 })
-
-
-// testing
